ba07de31934b687d8adaa82b3e999aa24a4f9363
Alp Dirikgil <alpdirikgil@gmail.com>
2022-12-04 06:37:57 +0100

    small beauty improvements

--- 2022/day4/day4.py
+++ 2022/day4/day4.py
@@ -4,7 +4,8 @@ with open('input.txt') as input_file:
     lines = [line.rstrip() for line in input_file]
 
 
-sum1 = 0
+contained = 0
+overlap = 0
 
 for line in lines:
     first_coord = line.split(",")[0]
@@ -16,11 +17,13 @@ for line in lines:
     sec_start = int(sec_coord.split("-")[0])
     sec_end   = int(sec_coord.split("-")[1])
 
-    # if first_start >= sec_start and first_end <= sec_end or sec_start >= first_start and sec_end <= first_end  for first part
+    if first_start >= sec_start and first_end <= sec_end or sec_start >= first_start and sec_end <= first_end:  # for first part
+        contained = contained + 1
 
-    if first_start >= sec_start and first_end <= sec_end or sec_start >= first_start and sec_end <= first_end or first_end >= sec_start and first_start <= sec_end or first_end >= sec_end and first_start <= sec_end:
-        sum1 = sum1 + 1
+    if first_end >= sec_start and first_start <= sec_end or first_end >= sec_end and first_start <= sec_end:
+        overlap = overlap + 1
 
     
     #print(first_start, first_end, sec_start, sec_end)
-    print(sum1)
+
+print("contained:", contained, "\noverlap:",overlap)

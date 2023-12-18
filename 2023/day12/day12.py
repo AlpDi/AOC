b75ce38ffc3c8fc91af2f89e851e95725b9dcd3f
alp <alpdirikgil@gmail.com>
2023-12-18 15:25:07 +0100

    finished day 12

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -1,47 +1,52 @@
 import fileinput
+from functools import cache
 
 
 
 testInput = "testinput.txt"
 input = "input.txt"
-arrangements = 0
 
-def get_arrangements(springs: list, groups):
-    print(springs, groups)
-    if not springs  and not groups: arrangements += 1
-    elif (not springs and groups) or (springs and not groups): return 0
-
+@cache
+def get_arrangements(springs, groups):
+    #print(springs, groups)
+    if not groups:
+        return 0 if "#" in springs else 1
+    if not springs:
+        return 1 if not groups else 0
     
-    cur = springs[0]
-    if cur == ".": 
-        springs.pop(0)
-        get_arrangements(springs, groups)
-    if cur == "?":
-        temp = springs
-        springs[0] = "."
-        get_arrangements(springs, groups)
-        temp[0] = "#"
-        get_arrangements(temp, groups)
-    if cur == "#":
-        if len(springs) >= groups[0]:
-            if all(x != "." for x in springs[:groups[0]-1]):
-                for i in range(groups[0]):
-                    springs.pop(i)
-                groups.pop(0)
-                get_arrangements(springs, groups)
     
+    result = 0
 
+    if springs[0] == '?' or springs[0] == '.':
+        result += get_arrangements(springs[1:], groups)
+    if springs[0] == '?' or springs[0] == '#':
+        if (
+            groups[0] <= len(springs)
+            and "." not in springs[: groups[0]]
+            and (groups[0] == len(springs) or springs[groups[0]] != '#')
+        ):
+            result += get_arrangements(springs[groups[0] + 1 :], groups[1:])
+
+    return result
 
 
 def main():
-    with open(testInput) as file:
+    with open(input) as file:
         lines = file.read().strip().split("\n")
+        arrangements = 0
+        arrangements2 = 0
         for line in lines:
             springs, groups = line.split(" ")
             groups = [int(x) for x in groups.split(",") if x != ""]
-            springs = [*springs]
-            get_arrangements(springs, groups)
-        print(arrangements)
+            groups = tuple(groups)
+            arrangements += get_arrangements(springs, groups)
+            #print(get_arrangements(springs, groups))
+
+            groups = groups * 5
+            springs = '?'.join([springs] * 5)
+            arrangements2 += get_arrangements(springs, groups)
+        print(arrangements, arrangements2)  
+            
 
 
 if __name__ == "__main__":

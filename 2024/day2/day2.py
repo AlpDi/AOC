554d52a710c94d0d026189900c620ae9fd060167
Alp <alpdirikgil@gmail.com>
2024-12-02 18:54:36 +0100

    finished day 2

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -1,21 +1,42 @@
+
+def is_safe(rep):
+    if rep == sorted(rep) or rep == sorted(rep)[::-1]:
+            pairs = list(zip(rep, rep[1:]))
+            diffs = list(map(lambda x: abs(x[0]-x[1]), pairs))
+            isSafe = list(map(lambda x: x < 4 and x > 0, diffs))
+            
+            return all(isSafe)
+
+
 def part1(data):
     counter = 0
     for entry in data:
         rep = [int(x) for x in entry.split()]
         
-        if rep == sorted(rep) or rep == sorted(rep)[::-1]:
-            pairs = list(zip(rep, rep[1:]))
-            diffs = list(map(lambda x: abs(x[0]-x[1]), pairs))
-            isSafe = list(map(lambda x: x < 4 and x > 0, diffs))
-            
-            if all(isSafe): counter += 1
-    return counter
-
+        if is_safe(rep):
+            counter += 1
 
     return counter
+
 
 def part2(data):
-    pass
+    counter = 0
+    for entry in data:
+        rep = [int(x) for x in entry.split()]
+        if not is_safe(rep):
+            for i in range(len(rep)):
+                temp = rep.copy()
+                del temp[i]
+                #print (temp)
+                if is_safe(temp):
+                    counter += 1
+                    break
+        else:
+            counter += 1
+
+    return counter
+            
+
 
 def parse_input(filename):
     with open(filename) as f:
